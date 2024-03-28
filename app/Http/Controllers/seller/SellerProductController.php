0a6e3dd08faec7b7780d74bc9258491eac815185
Roldan Jim Collado Valenzuela <chessmaster10.91@gmail.com>
2024-03-27 20:01:13 -0500

    feat: metodo store para almacenar productos por un vendedor

--- app/Http/Controllers/seller/SellerProductController.php
+++ app/Http/Controllers/seller/SellerProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\seller;
 
 use App\Http\Controllers\ApiController;
+use App\Product;
 use App\Seller;
+use App\User;
 use Illuminate\Http\Request;
 
 class SellerProductController extends ApiController
@@ -16,9 +18,34 @@ class SellerProductController extends ApiController
         return $this->showAll( $products );
     }
 
-    public function store(Request $request)
+    /**
+     * Metodo para almacenar productos.
+     * Cabe la posiblidad de que un usuario cualquiera, que aun no es vendedor quiera publicar un producto
+     * por primera vez. Qeuiere decir que en ese punto para el sistema aun no es un vendedor, es un usuario
+     * comun. Asi que en realidad no debemos pasar como parametro instancias de vendedores(Seller), sino
+     * de un usuario como tal, para que de esa manera facilitemos a que un usuario nuevo que nunca
+     * ah vendido algo, pueda publicar un producto por primera vez.
+     */
+    public function store(Request $request, User $seller)
     {
-        //
+        $rules = [
+            'name'          => 'required',
+            'description'   => 'required',
+            'quantity'      => 'required|integer|min:1',
+            'image'         => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create( $data );
+
+        return $this->showOne( $product, 201 );
     }
 
     public function update(Request $request, Seller $seller)
